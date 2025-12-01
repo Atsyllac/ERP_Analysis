c46e0d69f583f308b619e3281a4cd8dd36f20e9e
cal <100500379+Atsyllac@users.noreply.github.com>
2025-11-30 21:04:05 -0700

    Update and rename project_CNN.py to Model(s).py

--- Model(s).py
+++ Model(s).py
@@ -7,9 +7,7 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 
 
-
-
-npz = np.load("C:\\Users\\cally\\ProgramsAndProjects\\NN Hwk\\Project\\thealldata.npz")
+npz = np.load("C:\\Users\\cally\\ProgramsAndProjects\\NN Hwk\\Project\\thealldata.npz") #using absolute path
 
 eeg_test = npz["eeg_test"]
 eeg_train = npz["eeg_train"]
@@ -25,8 +23,9 @@ labels_test = npz["labels_test"]
 sentence_train_transposed = np.transpose(sentence_train, (0, 2, 1))
 sentence_test_transposed = np.transpose(sentence_test, (0, 2, 1))
 
-eeg_train_norm = (eeg_train - np.mean(eeg_train, axis=2, keepdims=True)) / (np.std(eeg_train, axis=2, keepdims=True) + 1e-9)
-eeg_test_norm  = (eeg_test  - np.mean(eeg_test,  axis=2, keepdims=True)) / (np.std(eeg_test,  axis=2, keepdims=True) + 1e-9)
+#normalizing eeg data, preventing 0s
+eeg_train_norm = (eeg_train - np.mean(eeg_train, axis=2, keepdims=True)) / (np.std(eeg_train, axis=2, keepdims=True) + 1e-10)
+eeg_test_norm  = (eeg_test  - np.mean(eeg_test,  axis=2, keepdims=True)) / (np.std(eeg_test,  axis=2, keepdims=True) + 1e-10)
 
 
 #-------------CNNs-------------#
@@ -40,6 +39,7 @@ x = layer.GlobalAveragePooling2D()(x)
 eeg_cnn_output = layer.Dense(256, activation='relu')(x)
 
 
+#this CNN must use Conv1D or things will be bad (Conv2D scrambles embeddings)
 embedding_CNN = layer.Input(shape=(10, 768))
 z = layer.Conv1D(filters=32, kernel_size=3, activation='relu', padding='same')(embedding_CNN)
 z = layer.BatchNormalization()(z)
@@ -62,7 +62,7 @@ embedding_cnn_output = layer.Dense(256, activation='relu')(z)
 concat_data = layer.Concatenate()([eeg_cnn_output, embedding_cnn_output])
 
 
-
+#-------------ANN Classifier-------------#
 ANN = layer.Dense(256, activation='relu')(concat_data)
 w = layer.Dense(256, activation='relu')(ANN)
 w = layer.Dropout(0.3)(w)
@@ -71,17 +71,17 @@ w = layer.Dropout(0.2)(w)
 ANN_output = layer.Dense(4, activation='softmax')(w)
 
 
-
 model = tf.keras.Model(inputs=[eeg_CNN, embedding_CNN], outputs=ANN_output)
 model.compile(
     loss='sparse_categorical_crossentropy',
     optimizer='adam',
     metrics=['accuracy']
 )
+model.summary()
 
-tf.debugging.set_log_device_placement(True)
+
 history = model.fit(
-    [eeg_train_norm,sentence_train_transposed],
+    [eeg_train_norm, sentence_train_transposed],
     labels_train,
     batch_size=64,
     epochs=10,
@@ -118,4 +118,5 @@ ax.set_ylabel('True labels')
 ax.set_title('Confusion Matrix')
 ax.xaxis.set_ticklabels(classes,rotation=90, fontsize = 18)
 ax.yaxis.set_ticklabels(classes,rotation=0, fontsize = 18)
+
 plt.show()
